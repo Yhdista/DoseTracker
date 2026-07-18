c3299fd32d98cba7ad1ff1c30bfcd7e8c50d0659
Jan Vlach <yhdista@gmail.com>
2026-07-18 19:48:48 +0200

    refactor: move domain layer to :shared, switch Dose/repository dates to kotlinx-datetime

--- shared/src/commonMain/kotlin/com/yhdista/dosetracker/domain/model/Dose.kt
+++ shared/src/commonMain/kotlin/com/yhdista/dosetracker/domain/model/Dose.kt
@@ -1,6 +1,6 @@
 package com.yhdista.dosetracker.domain.model
 
-import java.time.Instant
+import kotlinx.datetime.Instant
 
 data class Dose(
     val id: Long = 0,
--- shared/src/commonMain/kotlin/com/yhdista/dosetracker/domain/repository/MedicationRepository.kt
+++ shared/src/commonMain/kotlin/com/yhdista/dosetracker/domain/repository/MedicationRepository.kt
@@ -4,6 +4,7 @@ import com.yhdista.dosetracker.core.Data
 import com.yhdista.dosetracker.domain.model.Dose
 import com.yhdista.dosetracker.domain.model.Medication
 import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDate
 
 interface MedicationRepository {
     fun getMedications(): Flow<Data<List<Medication>>>
@@ -13,7 +14,7 @@ interface MedicationRepository {
     suspend fun deleteMedication(medication: Medication): Data<Unit>
 
     fun getDosesForMedication(medicationId: Long): Flow<Data<List<Dose>>>
-    fun getDosesForDate(date: java.time.LocalDate): Flow<Data<List<Dose>>>
+    fun getDosesForDate(date: LocalDate): Flow<Data<List<Dose>>>
     fun getAllDoses(): Flow<Data<List<Dose>>>
     suspend fun insertDose(dose: Dose): Data<Long>
     suspend fun updateDose(dose: Dose): Data<Unit>
